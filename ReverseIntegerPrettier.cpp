5d083abe36748f69bdf59bc1e1ff27e17e21f50d
CalebAlex-Ander <58803893+CalebAlex-Ander@users.noreply.github.com>
2020-10-27 00:45:23 -0500

    A shorter, prettier version of ReverseInteger
    Note the logic for checking for overflow!

---
+++ ReverseIntegerPrettier.cpp
@@ -0,0 +1,16 @@
+//Typed in notepad, some aesthetic issues
+
+class Solution {
+public:
+    int reverse(int x) {
+		int sol{0};
+		while (x != 0) {
+			int pop = x % 10;
+			x /= 10;
+			if (sol > INT_MAX/10 || (sol == INT_MAX / 10 && pop > 7)) return 0;
+            		if (sol < INT_MIN/10 || (sol == INT_MIN / 10 && pop < -8)) return 0;
+			sol = sol * 10 + pop;
+		}	
+		return sol;
+	}
+};
